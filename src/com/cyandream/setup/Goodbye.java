9f13752f72c0410057a722d99624763424feaa6a
yanniks <kontakt@yanniks.de>
2014-03-16 15:08:55 +0100

    some improvements and corrections
    Change-Id: I7d5f2a02a3250ca4a806e22be9fa8b0f5f6def52

--- src/com/cyandream/setup/Goodbye.java
+++ src/com/cyandream/setup/Goodbye.java
@@ -1,13 +1,44 @@
 package com.cyandream.setup;
 
 import android.app.Activity;
+import android.app.AppGlobals;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+import java.util.List;
 
 public class Goodbye extends Activity {
+    private static final String GOOGLE_SETUPWIZARD_PACKAGE = "com.google.android.setupwizard";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goodbye_setup);
+    }
+    public void finish (View view) {
+        Settings.Global.putInt(getContentResolver(), Settings.Global.DEVICE_PROVISIONED, 1);
+        Settings.Secure.putInt(getContentResolver(), Settings.Secure.USER_SETUP_COMPLETE, 1);
+        Intent intent = new Intent("android.intent.action.MAIN");
+        intent.addCategory("android.intent.category.HOME");
+        disableSetupWizards(intent);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | intent.getFlags());
+        startActivity(intent);
+        finish();
+    }
+    private void disableSetupWizards(Intent intent) {
+        final PackageManager pm = getPackageManager();
+        final List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo info : resolveInfos) {
+            if (GOOGLE_SETUPWIZARD_PACKAGE.equals(info.activityInfo.packageName)) {
+                final ComponentName componentName = new ComponentName(info.activityInfo.packageName, info.activityInfo.name);
+                pm.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+            }
+        }
+        pm.setComponentEnabledSetting(getComponentName(), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
     }
 }
